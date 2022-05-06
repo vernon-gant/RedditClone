0af5850d29957f4162e78909643a521b37e7ae16
Aleksandr Zakharov <aleksza.4119@gmail.com>
2022-05-06 12:07:15 +0200

    Created our entities and refactored using Project Lombok

---
+++ src/main/java/com/aleks/springit/domain/Link.java
@@ -0,0 +1,24 @@
+package com.aleks.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Link {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private String url;
+
+}
