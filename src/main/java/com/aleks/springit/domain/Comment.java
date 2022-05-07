f796463b3cb5d557dae6a549ea47f5fc4f20ba56
Aleksandr Zakharov <aleksza.4119@gmail.com>
2022-05-07 08:18:27 +0200

    Configured mappings from link -> comment.

--- src/main/java/com/aleks/springit/domain/Comment.java
+++ src/main/java/com/aleks/springit/domain/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.util.Objects;
 
 @Entity
@@ -17,5 +18,7 @@ public class Comment {
     @GeneratedValue
     private Long id;
     private String body;
+    @ManyToOne
+    private Link link;
 
 }
