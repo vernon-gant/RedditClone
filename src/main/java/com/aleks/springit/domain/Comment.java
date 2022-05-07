d1471fe66004dc9acbd8833d675843ec00a05d73
Aleksandr Zakharov <aleksza.4119@gmail.com>
2022-05-07 09:19:38 +0200

    Added abstract class Auditable and extending it in Link and & Comment

--- src/main/java/com/aleks/springit/domain/Comment.java
+++ src/main/java/com/aleks/springit/domain/Comment.java
@@ -7,12 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import java.util.Objects;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Comment {
+public class Comment extends Auditable {
 
     @Id
     @GeneratedValue
--- src/main/java/com/aleks/springit/repository/LinkRepository.java
+++ src/main/java/com/aleks/springit/repository/LinkRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LinkRepository extends JpaRepository<Link,Long> {
 
-    Link findByTitle(String title);
-
 }
